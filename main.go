86a0333f707ef8509b490d7c4d1f64c0fc8d174b
Dharaneeshwaran <dharanish185@gmail.com>
2021-10-10 19:20:34 +0530

    Removed print

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
 	config := initConfig()
 	var KubeClient KubeClient
 	KubeClient.initKubeClient(config.SourceKubeConfFile, "source")
 	KubeClient.initKubeClient(config.TargetKubeConfFile, "target")
-	fmt.Println(config.SourceNameSpace, config.TargetNameSpace)
+
 	KubeClient.SourceNameSpace = config.SourceNameSpace
 	KubeClient.TargetNameSpace = config.TargetNameSpace
 
